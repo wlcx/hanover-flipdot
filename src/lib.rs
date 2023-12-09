128a5e3d6115c7b6c09159d88c7e09c68d2d2fb5
Sam Willcocks <sam@wlcx.cc>
2023-12-09 21:47:59 +0000

    Idk make it work?

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
-use std::{error::Error, io::Write};
+#![no_std]
+
+#[cfg(feature = "std")]
+extern crate std;
+use std::io::Write;
+use std::boxed::Box;
+use std::vec;
+
+use core_error::Error;
 
 use bitvec::prelude::*;
+#[cfg(feature = "embedded-graphics")]
 use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
 
 const HEX: &[u8; 16] = b"0123456789ABCDEF";
 
+// Turn one byte into its two byte hexadecimal representation.
+// Yeah, there are crates for this but it's so simple to do...
 fn byte_to_hex(b: &u8) -> [u8; 2] {
     [HEX[((*b & 0xf0) >> 4) as usize], HEX[(*b & 0xf) as usize]]
 }
@@ -17,6 +28,9 @@ pub struct HanoverFlipdot {
 }
 
 impl HanoverFlipdot {
+    /// Initialise a new Flipdot with the given width, height and address.
+    /// The address is the value selected on the address rotary switch on the flipdot
+    /// controller PCB.
     pub fn new(w: u32, h: u32, addr: u8) -> Self {
         assert!(addr < 16);
         Self {
@@ -26,6 +40,36 @@ impl HanoverFlipdot {
             framebuffer: bitvec![u8, Lsb0; 0; (w*h) as usize],
         }
     }
+
+    pub fn get_packet_size(&self) -> usize {
+        // One bit per pixel, plus addr, resolution and checksum bytes
+        (((self.h * self.w)/8 + 3)
+        * 2 // Encoded as hex (1 byte == 2 ascii hex bytes)
+        + 2) // Plus start and end bytes
+        as usize
+    }
+
+    /// Encode the current frame buffer contents into a serial protocol message.
+    /// `buf` must have >= `get_packet_size()` of capacity.
+    pub fn make_packet(&self, buf: &mut [u8]) {
+        let mut i = 0;
+        buf[i] = 0x2;
+        i += 1;
+        for b in [self.addr + 17, ((self.h * self.w) / 8) as u8]
+        .iter()
+        .chain(self.framebuffer.as_raw_slice().into_iter())
+        .flat_map(byte_to_hex) {
+            buf[i] = b;
+            i+=1;
+        }
+        buf[i] = 0x03;
+        let sum = buf[1..=i].iter().map(|x| *x as u64).sum::<u64>() as u8;
+        i += 1;
+        [buf[i], buf[i+1]] = byte_to_hex(&((sum ^ 0xFF).wrapping_add(1)));
+    }
+
+    #[cfg(feature = "std")]
+    /// Encode the frame and write it to `writer`
     pub fn write_frame<W>(&self, writer: &mut W) -> Result<(), Box<dyn Error>>
     where
         W: Write,
@@ -33,6 +77,7 @@ impl HanoverFlipdot {
         // Encode the address and resolution.
         // The address is the number on the address selector on the flipdot PCB + 17. For
         // some reason.
+
         let mut data = vec![0x2];
         data.extend(
             [self.addr + 17, ((self.h * self.w) / 8) as u8]
@@ -46,8 +91,23 @@ impl HanoverFlipdot {
         writer.write(&data)?;
         Ok(())
     }
+
+    fn write_vec(&self) -> vec::Vec<u8> {
+        let mut data = vec![0x2];
+        data.extend(
+            [self.addr + 17, ((self.h * self.w) / 8) as u8]
+                .iter()
+                .chain(self.framebuffer.as_raw_slice().into_iter())
+                .flat_map(byte_to_hex),
+        );
+        data.push(0x03);
+        let sum = data.iter().skip(1).map(|x| *x as u64).sum::<u64>() as u8;
+        data.extend(byte_to_hex(&((sum ^ 0xFF).wrapping_add(1))));
+        data
+    }
 }
 
+#[cfg(feature = "embedded-graphics")]
 impl DrawTarget for HanoverFlipdot {
     type Color = BinaryColor;
     type Error = core::convert::Infallible;
@@ -67,6 +127,7 @@ impl DrawTarget for HanoverFlipdot {
     }
 }
 
+#[cfg(feature = "embedded-graphics")]
 impl OriginDimensions for HanoverFlipdot {
     fn size(&self) -> Size {
         Size::new(self.w, self.h)
@@ -82,5 +143,15 @@ mod tests {
         assert_eq!(byte_to_hex(&192), *b"C0");
         assert_eq!(byte_to_hex(&0), *b"00");
         assert_eq!(byte_to_hex(&255), *b"FF");
+    }
+
+    #[test]
+    fn lol() {
+        let fd = HanoverFlipdot::new(96, 16, 0);
+        let one = fd.write_vec();
+        assert!(one.len() == fd.get_packet_size());
+        let mut two = vec!(0; 2048);
+        fd.make_packet(&mut two);
+        assert_eq!(one, two);
     }
 }
